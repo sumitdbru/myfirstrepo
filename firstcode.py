ffa31f294b8aaded72353f61827b0f981c6596b0
sumitdbru <rajsumit242005@gmail.com>
2024-04-12 14:52:06 +0530

    first commit

---
+++ firstcode.py
@@ -0,0 +1,28 @@
+# x = 5 #int
+# y = "John" # string
+# z = True #boolean
+# m = 3.3 # float
+# # print(type(x))
+# # print(type(y))
+# # print(type(z))
+# # print(type(m))
+
+# x,t,k = 5,7,5 # miultivariablea assignemnet 
+
+# print(x,t,k,z,y)
+
+#global variable 
+
+x = "awesome" # global variable
+
+def sumitfunc(s):  # declare the functin
+   x = "fantastic" #local variable 
+   print("Python is " + x + s)
+
+sumitfunc("kd") # call the funtion 
+
+name = "sumit xyz is  " + x
+
+#name = "ravi xyz"
+
+print(name)
